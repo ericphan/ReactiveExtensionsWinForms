7e2f79f589d6cd1db16c0565d7879affbf03a549
Eric Phan <eric.phan@practiceevolve.com.au>
2020-02-07 17:34:27 +1100

    Initial Demo Commit

---
+++ RxWinFormsSample/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace RxWinFormsSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.buttonBackgroundJob = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(418, 12);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "&Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 41);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(480, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // txtResults
+            // 
+            this.txtResults.Location = new System.Drawing.Point(12, 70);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.Size = new System.Drawing.Size(481, 268);
+            this.txtResults.TabIndex = 2;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(13, 15);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(399, 20);
+            this.txtSearch.TabIndex = 3;
+            // 
+            // buttonBackgroundJob
+            // 
+            this.buttonBackgroundJob.Location = new System.Drawing.Point(499, 12);
+            this.buttonBackgroundJob.Name = "buttonBackgroundJob";
+            this.buttonBackgroundJob.Size = new System.Drawing.Size(118, 23);
+            this.buttonBackgroundJob.TabIndex = 4;
+            this.buttonBackgroundJob.Text = "#1";
+            this.buttonBackgroundJob.UseVisualStyleBackColor = true;
+            this.buttonBackgroundJob.Click += new System.EventHandler(this.buttonBackgroundJob_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(499, 283);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "&Search";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(499, 312);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "&Search";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(499, 41);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(499, 64);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Enabled = false;
+            this.checkBox3.Location = new System.Drawing.Point(499, 87);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(80, 17);
+            this.checkBox3.TabIndex = 9;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Enabled = false;
+            this.checkBox4.Location = new System.Drawing.Point(499, 110);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(80, 17);
+            this.checkBox4.TabIndex = 10;
+            this.checkBox4.Text = "checkBox4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 345);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonBackgroundJob);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox txtResults;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button buttonBackgroundJob;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+    }
+}
+
---
+++ RxWinFormsSample/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RxWinFormsSample
+{
+    public partial class Form1 : Form
+    {
+        IObservable<string> _filtersChanged;
+        IObservable<bool> _checkBox2Changed;
+
+        IObservable<string> _usageCollector;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            var checkbox1CheckChanges = Observable.FromEventPattern(
+                ev => checkBox1.CheckedChanged += ev,
+                ev => checkBox1.CheckedChanged -= ev
+                );
+
+
+
+            var checkBox1Changed = Observable.FromEventPattern(ev => checkBox1.CheckedChanged += ev, ev => checkBox1.CheckedChanged -= ev)
+                .Select(ev => ((CheckBox)ev.Sender).Checked)
+                .StartWith(false);
+            _checkBox2Changed = Observable.FromEventPattern(ev => checkBox2.CheckedChanged += ev, ev => checkBox2.CheckedChanged -= ev).Select(ev => ((CheckBox)ev.Sender).Checked).StartWith(false);
+            var checkBox3Changed = Observable.FromEventPattern(ev => checkBox3.CheckedChanged += ev, ev => checkBox3.CheckedChanged -= ev).Select(ev => ((CheckBox)ev.Sender).Checked).StartWith(false);
+            var checkBox4Changed = Observable.FromEventPattern(ev => checkBox4.CheckedChanged += ev, ev => checkBox4.CheckedChanged -= ev).Select(ev => ((CheckBox)ev.Sender).Checked).StartWith(false);
+            var searchTextChanged = Observable.FromEventPattern(ev => txtSearch.TextChanged += ev, ev => checkBox4.TextChanged -= ev).Select(ev => ((TextBox)ev.Sender).Text).StartWith(string.Empty);
+
+            _filtersChanged = Observable.CombineLatest(
+                searchTextChanged, 
+                checkBox1Changed, 
+                _checkBox2Changed, 
+                checkBox3Changed, 
+                checkBox4Changed, 
+                (text, c1, c2, c3, c4) =>
+                {
+                    return $"Keyword: {text}, Filter 1: {c1}, Filter 2: {c2}, Filter 3: {c3}, Filter 4: {c4}";
+                })
+            .Throttle(TimeSpan.FromSeconds(.5))
+            ;
+
+            _usageCollector = Observable.Merge(
+                checkBox1Changed.Select(_ => "Checkbox 1"),
+                _checkBox2Changed.Select(_ => "Checkbox 2"),
+                checkBox3Changed.Select(_ => "Checkbox 3"),
+                checkBox4Changed.Select(_ => "Checkbox 4"),
+                searchTextChanged.Select(_ => "Search Textbox"),
+                Observable.FromEventPattern(ev => btnSearch.Click += ev, ev => btnSearch.Click -= ev).Select(_ => "Search Button"));
+
+        }
+
+        private void buttonBackgroundJob_Click(object sender, EventArgs e)
+        {
+            var o = Observable.StartAsync(async () =>
+            {
+                txtResults.Text = "I'm doing something";
+                progressBar1.Style = ProgressBarStyle.Marquee;
+                return Task.Delay(3000);
+            });
+
+            o.Subscribe(_ =>
+            {
+                progressBar1.Style = ProgressBarStyle.Continuous;
+                txtResults.Text += Environment.NewLine + "I'm done";
+            }); 
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            // Handle filter changes to trigger search
+            _filtersChanged
+                .ObserveOn(this)
+                .Subscribe(options =>
+            {
+                txtResults.Text += options + Environment.NewLine;
+            }, err =>
+            {
+                txtResults.Text += "Error" + Environment.NewLine;
+            });
+
+
+
+            //  Handle Enabling and Disabling of Filter Checkboxes
+            _checkBox2Changed.ObserveOn(this).Subscribe(@checked =>
+            {
+                checkBox3.Enabled = @checked;
+                checkBox4.Enabled = @checked;
+            });
+
+
+            // Collect usage stats
+            _usageCollector.Timestamp()
+                .Select(x => $"{x.Timestamp}: {x.Value}")
+                .Buffer(TimeSpan.FromSeconds(5))
+                .Where(usage => usage.Any())
+                .ObserveOn(this)
+                .Subscribe(senders =>
+                {
+                    txtResults.Text += "Sending usage stats [" + string.Join(",", senders) + "]" + Environment.NewLine;
+                });
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            // Trigger search
+
+            // Enable disable stuff
+
+            //fs
+        }
+    }
+}
